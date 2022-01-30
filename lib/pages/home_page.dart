bedb74f035aba8a36e3f1740d6f1f2415628197e
Mayank Agarwal <agarwalmayank319@gmail.com>
2022-01-30 13:34:25 +0530

    Saathi 2.0

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,41 +1,71 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:sathi/pages/camera.dart';
+import 'package:sathi/pages/history.dart';
+import 'package:sathi/pages/voice.dart';
 import 'package:sathi/utils/drawer.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:velocity_x/velocity_x.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  static List<Widget> _widgetOptions = <Widget>[
+    Camera(),
+    VoicePage(),
+    History(),
+  ];
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: CurvedNavigationBar(
-        backgroundColor: const Color(0xffff6387),
-        items: <Widget>[
-          Icon(Icons.camera_alt_outlined, size: 30),
-          Icon(
-            CupertinoIcons.mic,
-            size: 30,
-            semanticLabel: "Speak Now",
-          ),
-          Icon(
-            Icons.library_books_outlined,
-            size: 30,
-          ),
-        ],
-        onTap: (index) {
-          //Handle button tap
-        },
-      ),
       appBar: AppBar(
-        title: Text("Saathi"),
-        backgroundColor: const Color(0xffff6387),
+        backgroundColor: Color(0xffff6387),
+        title: "Saathi".text.xl3.make(),
       ),
       backgroundColor: Colors.white,
       body: Center(
-        child: ButtonBar(
-          buttonHeight: 20,
-        ),
+        child: HomePage._widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              CupertinoIcons.camera,
+              size: 30,
+            ),
+            label: 'Camera',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              CupertinoIcons.mic,
+              size: 50,
+            ),
+            label: 'Voice',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              CupertinoIcons.doc_plaintext,
+              size: 30,
+            ),
+            label: 'History',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Color(0xffff6387),
+        onTap: _onItemTapped,
       ),
       drawer: MyDrawer(),
     );
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:sathi/pages/home_page.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -78,7 +79,14 @@ class _LoginPageState extends State<LoginPage> {
                   "Login",
                   style: TextStyle(fontSize: 20),
                 ),
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => HomePage(),
+                    ),
+                  );
+                },
               ),
             ),
             SizedBox(
